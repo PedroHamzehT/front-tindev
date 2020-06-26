b2adf9026ad601d8dea349fed90d0fcf4061a911
pedro@9jobs <pedro@99jobs.com>
2020-06-25 23:13:23 -0300

    Add the routes flow to the project and start to use the props and state in the Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './Login.css';
+
+import api from '../services/api';
+
 import tindev from '../assets/tindev.svg';
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/devs', {
+      username
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={tindev} alt="logo" id="tindev-logo"/>
-        <input placeholder="Digite seu usuário do github" />
+        <input placeholder="Digite seu usuário do github"
+        value={username}
+        onChange={event => setUsername(event.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
